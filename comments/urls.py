4cbb2f94a2a194ca05097e1993e09299820c8029
hyeop kang <hyeopkang@MacBookProHyeop.local>
2020-10-23 02:50:48 +0900

    [Changed/Hyeop] Comment Structure

--- comments/urls.py
+++ comments/urls.py
@@ -4,5 +4,5 @@ from .views import *
 app_name = "comments"   
    
 urlpatterns = [
-    path('<int:content_id>/create_comment/', create_comment, name="create_comment" ),
+    #path('create_comment/', create_comment, name="create_comment" ),
 ]
--- comments/views.py
+++ comments/views.py
@@ -2,19 +2,16 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Comment
 from django.http import HttpResponse
 import random, requests, pdb, ast, json
+import logging
 
-# Create your views here.
-def create_comment(request, content_id):
+# for console log
+logger = logging.getLogger(__name__)
+
+logger.error("something wrong")
+
+# data post by REST API
+def create_comment(request, content):
     if request.method == "POST":
         content = ast.literal_eval(request.POST.get('content'))
         message = request.POST.get('message')
-        identity = content['identity'] 
-        # current_user = "익명"
-        # comment_content = request.POST.get('content')
-        # comment = Comment.objects.create(text=message)
-        context = {
-            'content': content,
-            'message': message,
-            'identity': identity,
-        }
-    return HttpResponse(json.dumps(context), content_type="application/json")
+        identity = content['identity']
